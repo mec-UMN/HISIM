80f834df53f5babf91b34142e6c82883f0060d6f
nalla052 <nalla052@umn.edu>
2025-10-07 18:18:44 -0500

    Updated config.py with 2D_Mesh as default

--- HISIM-SystolicArray/config.py
+++ HISIM-SystolicArray/config.py
@@ -3,10 +3,10 @@ import os
 DEBUG=False
 CREATE_DEFAULT_FILES=True
 #TYPE_DEFAULT_FILES="2_5D_Mesh"
-#TYPE_DEFAULT_FILES="2D_Mesh"
+TYPE_DEFAULT_FILES="2D_Mesh"
 #TYPE_DEFAULT_FILES="3_5D_Mesh"
 #TYPE_DEFAULT_FILES="2_5D_Mesh_Scaled"
-TYPE_DEFAULT_FILES="3_5D_Mesh_Scaled"
+#TYPE_DEFAULT_FILES="3_5D_Mesh_Scaled"
 SET_SUFF_BANKS=True
 aimodel="Attention"
 main_dir = os.path.dirname(__file__)
